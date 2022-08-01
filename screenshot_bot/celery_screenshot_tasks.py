7c062a0e6dbc5bdbab4c28d9abbf66687ebf8254
Alexey Yakovlev <Yakobro@mail.ru>
2022-08-01 03:46:26 +0300

    Init telefram_bot

---
+++ screenshot_bot/celery_screenshot_tasks.py
@@ -0,0 +1,112 @@
+from celery import Celery
+from selenium import webdriver
+from datetime import datetime
+import telebot
+from telebot import types
+import logging
+import os
+
+
+from dotenv import load_dotenv
+load_dotenv()
+
+import re
+url_pattern_regex = '(([\da-zA-Z])([_\w-]{,62})\.){,127}(([\da-zA-Z])[_\w-]{,61})?([\da-zA-Z]\.((xn\-\-[a-zA-Z\d]+)|([a-zA-Z\d]{2,})))'
+
+logging.basicConfig(filename='celery_log', level=logging.INFO)
+app = Celery('tasks', broker=os.getenv('BROKER_CELERY'), backend=os.getenv('BACKEND_CELERY'))
+
+
+@app.task
+def screen_create(msg_info, url):
+    """
+    С помощью selenium через Chromedriver делает снимок сайта.
+
+    :param msg_info: Данные о полученном сообщении от пользователя {chat_id, message_id, date(date received message)}
+    :param url: URL полученный от пользователя
+    :return: Возвратом вызывает функцию send_message_result() отправки\редактирования сообщения с фото сайта
+    """
+    start_time = datetime.now()
+
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless")
+    options.add_argument('--no-sandbox')
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument('--remote-debugging-port=9222')
+    driver = webdriver.Chrome(chrome_options=options)
+    # Если выгрузка продлилась дольше 30 сек. то выдать ошибку
+    driver.set_page_load_timeout(30)
+    driver.set_window_size(1920, 1080)
+
+    try:
+        driver.get(url)
+        url = driver.current_url
+        domain = re.search(url_pattern_regex, url).group(9)
+        domain_name = re.search(url_pattern_regex, url).group(0)
+
+        filename = datetime.utcfromtimestamp(msg_info.get('date') + 10800).strftime('%H-%M-%S_%d-%m-%Y') + "_" + \
+                   str(msg_info.get('chat_id')) + "_" + f'.{domain}' + '.png'
+
+        screen_directory_save = f"save_screenshots_{datetime.now().strftime('%d-%m-%Y')}"
+        if not os.path.exists(screen_directory_save):
+            os.makedirs(screen_directory_save)
+        driver.save_screenshot(f'./{screen_directory_save}/' + filename)
+        title_site = driver.title
+        driver.close()
+
+        screen = f'./{screen_directory_save}/' + filename
+        stop_time = (datetime.now() - start_time).seconds
+
+        response = {
+            'title': f"<b>{title_site}</b>",
+            'screen': screen,
+            'url': url,
+            'processing_time': f"<b>Время обработки: </b>{stop_time} секунд",
+            'chat_id': msg_info.get('chat_id'),
+            'message_id': msg_info.get('message_id'),
+            'domain_name': domain_name
+        }
+    except:
+        logging.exception('exception')
+        response = {
+            'title': "Что то не так с ссылкой...\nПревышено время ожидания ответа.",
+            'screen': './error.PNG',
+            'url': url,
+            'processing_time': '',
+            'chat_id': msg_info.get('chat_id'),
+            'message_id': msg_info.get('message_id'),
+        }
+    return send_message_result(response)
+
+
+def send_message_result(screen):
+    """
+    Редактирует предыдущее сообщение пользователя с URL, добобляя фото сайта и кнопку "Подробнее" для получения
+    whois об сайте.
+
+    В случае какой либо ошибки, возвращает сообщение об ошибке. Бед whois.
+
+    :param screen: Данные о сайте. {title:заголовок_сайта, screen:фото, url:ссылка,
+    processing_time:время_выполния_проуесса, chat_id:id, message_id:id}
+    :return: DONE!
+    """
+    bot = telebot.TeleBot(os.getenv('TOKEN'))
+
+    markup = None
+    if screen.get('domain_name') is not None:
+        markup = types.InlineKeyboardMarkup()
+        button1 = types.InlineKeyboardButton(text="Подробнее", callback_data=screen.get('domain_name'))
+        markup.add(button1)
+
+    bot.edit_message_media(
+        media=types.InputMediaPhoto(
+            media=open(screen.get('screen'), 'rb'),
+            caption=f"{screen.get('title')}\n\n<b>Веб-сайт: </b>{screen.get('url')}\n{screen.get('processing_time')}",
+            parse_mode='HTML',
+        ),
+        chat_id=screen.get('chat_id'),
+        message_id=screen.get('message_id'),
+        reply_markup=markup,
+    )
+
+    return 'DONE!'
---
+++ screenshot_bot/screenshots_bot.py
@@ -0,0 +1,84 @@
+from telebot.async_telebot import AsyncTeleBot
+import asyncio
+from telebot import types
+from celery_screenshot_tasks import screen_create
+import validators
+import whois
+import socket
+import logging
+import os
+
+from dotenv import load_dotenv
+load_dotenv()
+
+import re
+url_pattern_regex = '(([\da-zA-Z])([_\w-]{,62})\.){,127}(([\da-zA-Z])[_\w-]{,61})?([\da-zA-Z]\.((xn\-\-[a-zA-Z\d]+)|([a-zA-Z\d]{2,})))'
+
+logging.basicConfig(filename='bot_log', level=logging.INFO)
+
+bot = AsyncTeleBot(os.getenv('TOKEN'))
+
+start_text = """👋🏻 Привет! Меня зовут <b>FrontShot.</b>
+Я - Бот для создания веб-скриншотов.
+Чтобы получить скриншот - отправьте URL адрес сайта. Например, wikipedia.org
+
+• С помощью бота вы можете проверять подозрительные ссылки. <i>(Айпилоггеры, фишинговые веб-сайты, скримеры и т.п)</i>
+
+• Вы также можете добавить меня в свои чаты, и я смогу проверять ссылки, которые отправляют пользователи.
+
+ <b>FrontShot</b> использует <b>chromedriver.</b>
+Работает с протоколами <b>http, https.</b>
+
+<s>И находится в постоянной разработке.</s>"""
+
+markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+btn_start = types.KeyboardButton('/start')
+markup.add(btn_start)
+
+
+@bot.message_handler(commands=['start', 'help'])
+async def start_message(message):
+    await bot.send_message(message.chat.id, start_text, parse_mode="HTML", reply_markup=markup)
+
+
+@bot.message_handler(content_types='text')
+async def screenshot_url(message):
+    """
+    Получив сообщение, проверяет что это URL и передаёт необходимые данные для создания снимка сайта в celery.
+    :param message: инфо о сообщении.
+    :return: None
+    """
+    url = re.search(url_pattern_regex, message.text.split(' ')[0]).group(0)
+    if validators.domain(url):
+        url = 'http://' + url
+        res = await bot.send_animation(
+            chat_id=message.chat.id,
+            animation=open('please-wait.mp4', 'rb'),
+            caption="⚡️Ваш запрос обрабатывается..."
+        )
+        info_msg = {'date': res.date, 'chat_id': res.chat.id, 'message_id': res.message_id}
+
+        # Отправка в задачу celery, через redis
+        screen_create.delay(info_msg, url)
+
+
+@bot.callback_query_handler(func=lambda call: True)
+async def whois_msg(message):
+    """
+    Функция обрабатывает нажатие по кнопке "Подробнее" и отображает WHOIS о полученной ссылке в сообщении.
+    :param message: инфо о сообщении.
+    :return: None
+    """
+    whois_info = whois.whois(message.data)
+    ip_who = socket.gethostbyname(whois_info.get('domain_name') if isinstance(whois_info.get('domain_name'), str) else
+                                  whois_info.get('domain_name')[0])
+
+    msg = f"ip: {str(ip_who)}\n" \
+          f"Имя домена: {whois_info.get('domain_name')}\n" \
+          f"Организация: {whois_info.get('org')}\n" \
+          f"Страна: {whois_info.get('country')}"
+
+    await bot.answer_callback_query(callback_query_id=message.id, text=msg, show_alert=True)
+
+
+asyncio.run(bot.polling(non_stop=True))
